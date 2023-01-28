424e809275e64375dd9e2c7b0d9413e99cade8e2
siadati <msiyadati7779@gamil.com>
2023-01-28 11:08:38 +0330

    set function button check

--- starter/script.js
+++ starter/script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+let randomNumber = Math.trunc(Math.random() * 20 + 1);
+console.log('randomNumber:', randomNumber);
+let message = (document.querySelector('.message').textContent =
+  'Start guessing...');
+let secretNumber = (document.querySelector('.number').textContent =
+  randomNumber);
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+});
